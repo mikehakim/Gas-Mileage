9c7798553a86cd0e2c6e0d3d1996e2d462b0d9fc
Michael Hakim <hakimmike@outlook.com>
2015-07-14 13:35:43 -0700

    new table.php

--- table.php
+++ table.php
@@ -1,18 +1,46 @@
+<html>
+<body>
+<style type="text/css">
+    th,td{
+        border-width:0px 1px 1px 0px;
+        width: 370px;
+        color: purple;
+        font-family: Tahoma;
+        border-color: olive;
+        background:lightcyan;
+    }
+
+
+
+
+</style>
+
 <?php
 
-
-$file = "gasoline.txt";
-
-$content = file_get_contents($file);
+include 'dbconnection.php';
 
 
+// result = query
+// row = res
 
-$lines = explode("\n", $content);
+$sql = "SELECT numberOfGallons, milesTravelled, gasMileage, currentDate FROM gascalc";
+$result = $link->query($sql);
 
-foreach ($lines as $newline)  {
+if ($result->num_rows > 0) {
 
-    echo $newline . '<br>';
+    echo'<table border="1"><th > Number of Gallons </th><th> Miles Travelled </th><th> Gas Mileage </th> <th>Date</th>';
+
+    while($row = $result->fetch_assoc() ) {
+
+        echo "<tr><td>" . $row['numberOfGallons']. "</td><td>" . $row['milesTravelled']. "</td><td> " . $row['gasMileage']. "</td><td>" . $row['currentDate'] .  "</td></tr>";
+
 }
+    echo "</table>";
+}
+
+
+
+$link->close();
 
 
 ?>
